562abb37abdd5b967e4243a23bc328fd03251005
Luiz Carlos Silvestrini <luizsilvestrini15@hotmail.com>
2023-02-27 19:26:59 -0300

    Product listing from back end

--- frontend/src/pages/Admin/Products/Form/index.tsx
+++ frontend/src/pages/Admin/Products/Form/index.tsx
@@ -1,45 +1,46 @@
-import './styles.css'
+import './styles.css';
 
 const Form = () => {
+  return (
+    <div className="product-crud-container">
+      <div className="base-card product-crud-form-card">
+        <h1 className="product-crud-form-title">DADOS DO PRODUTO</h1>
 
-    return (
-        <div className="product-crud-container">
-            <div className="base-card product-crud-form-card">
-            <h1 className="product-crud-form-title">DADOS DO PRODUTO</h1>
-        
-            <form action="">
-                <div className="row product-crud-inputs-container">
-                <div className="col-lg-6 product-crud-inputs-left-container">
-                    <div className="margin-bottom-30">
-                    <input type="text" className="form-control base-input" />
-                    </div>
-                    <div className="margin-bottom-30">
-                    <input type="text" className="form-control base-input" />
-                    </div>
-                    <div>
-                    <input type="text" className="form-control base-input" />
-                    </div>
-                  
-                </div>
-                <div className="col-lg-6">
-                <div>
-                    <textarea
-                     name=""
-                     className="form-control base-input h-auto"
-                    rows={10}
-                    />
-                    </div>
-                </div>
+        <form action="">
+          <div className="row product-crud-inputs-container">
+            <div className="col-lg-6 product-crud-inputs-left-container">
+              <div className="margin-bottom-30">
+                <input type="text" className="form-control base-input" />
+              </div>
+              <div className="margin-bottom-30">
+                <input type="text" className="form-control base-input" />
+              </div>
+              <div>
+                <input type="text" className="form-control base-input" />
+              </div>
             </div>
-            <div className="product-crud-buttons-container">
-                <button className="btn btn-outline-danger product-crud-buttons">CANCELAR</button>
-                <button className="btn btn-primary product-crud-buttons text-white">SALVAR</button>
+            <div className="col-lg-6">
+              <div>
+                <textarea
+                  name=""
+                  className="form-control base-input h-auto"
+                  rows={10}
+                />
+              </div>
             </div>
-            </form>
-        
-        </div>
+          </div>
+          <div className="product-crud-buttons-container">
+            <button className="btn btn-outline-danger product-crud-buttons">
+              CANCELAR
+            </button>
+            <button className="btn btn-primary product-crud-buttons text-white">
+              SALVAR
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
-    );
+  );
 };
 
 export default Form;
