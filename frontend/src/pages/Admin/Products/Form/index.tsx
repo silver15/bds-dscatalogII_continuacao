e3f2a1856dcd746bc50a452c00614418f03e8d95
Luiz Carlos Silvestrini <luizsilvestrini15@hotmail.com>
2023-03-05 17:52:18 -0300

    float format

--- frontend/src/pages/Admin/Products/Form/index.tsx
+++ frontend/src/pages/Admin/Products/Form/index.tsx
@@ -1,5 +1,6 @@
 import { AxiosRequestConfig } from 'axios';
 import { useEffect, useState } from 'react';
+import CurrencyInput from 'react-currency-input-field';
 import { useForm, Controller } from 'react-hook-form';
 import { useHistory, useParams } from 'react-router-dom';
 import Select from 'react-select';
@@ -50,13 +51,15 @@ const Form = () => {
   }, [isEditing, productId, setValue]);
 
   const onSubmit = (formData: Product) => {
-    
-
+    const data = {
+      ...formData,
+      price: String(formData.price).replace(',','.'),
+    };
 
     const config: AxiosRequestConfig = {
       method: isEditing ? 'PUT' : 'POST',
       url: isEditing ? `/products/${productId}` : '/products',
-      data: formData,
+      data,
       withCredentials: true,
     };
 
@@ -101,7 +104,8 @@ const Form = () => {
                     rules={{ required: true }}
                     control={control}
                     render={({ field }) => (
-                      <Select {...field}
+                      <Select
+                        {...field}
                         options={selectCategories}
                         classNamePrefix="product-crud-select"
                         isMulti
@@ -114,21 +118,27 @@ const Form = () => {
                   />
                   {errors.categories && (
                     <div className="invalid-feedback d-block">
-                      Campo obrigatório</div>
+                      Campo obrigatório
+                    </div>
                   )}
                 </div>
 
-                <div className="mb-4">
-                  <input
-                    {...register('price', {
-                      required: 'Campo obrigatório',
-                    })}
-                    type="text"
-                    className={`form-control base-input ${
-                      errors.name ? 'is-invalid' : ''
-                    }`}
-                    placeholder="Preço"
+                <div className="margin-botton-30">
+                  <Controller
                     name="price"
+                    rules={{ required: 'Campo obrigatório' }}
+                    control={control}
+                    render={({ field }) => (
+                      <CurrencyInput
+                        placeholder="Preço"
+                        className={`form-control base-input ${
+                          errors.name ? 'is-invalid' : ''
+                        }`}
+                        disableGroupSeparators={true}
+                        value={field.value}
+                        onValueChange={field.onChange}
+                      />
+                    )}
                   />
                   <div className="invalid-feedback d-block">
                     {errors.price?.message}
@@ -140,9 +150,9 @@ const Form = () => {
                     {...register('imgUrl', {
                       required: 'Campo obrigatório',
                       pattern: {
-                        value:/^(https?|chrome):\/\/[^\s$.?#].[^\s]*$/gm,
-                        message: 'Deve ser uma URL válida'
-                      }
+                        value: /^(https?|chrome):\/\/[^\s$.?#].[^\s]*$/gm,
+                        message: 'Deve ser uma URL válida',
+                      },
                     })}
                     type="text"
                     className={`form-control base-input ${
@@ -155,8 +165,6 @@ const Form = () => {
                     {errors.imgUrl?.message}
                   </div>
                 </div>
-
-
               </div>
             </div>
             <div className="col-lg-6">
