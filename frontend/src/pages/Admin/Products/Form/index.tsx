1f8b28b83599b60ab86e0835509826ff20ebad7a
Luiz Carlos Silvestrini <luizsilvestrini15@hotmail.com>
2023-03-05 15:39:40 -0300

    Refactor categories/imgUrl field

--- frontend/src/pages/Admin/Products/Form/index.tsx
+++ frontend/src/pages/Admin/Products/Form/index.tsx
@@ -50,18 +50,13 @@ const Form = () => {
   }, [isEditing, productId, setValue]);
 
   const onSubmit = (formData: Product) => {
-    const data = {
-      ...formData,
-      imgUrl: isEditing
-        ? formData.imgUrl
-        : 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg',
-      categories: isEditing ? formData.categories : [{ id: 1, name: '' }],
-    };
+    
+
 
     const config: AxiosRequestConfig = {
       method: isEditing ? 'PUT' : 'POST',
       url: isEditing ? `/products/${productId}` : '/products',
-      data,
+      data: formData,
       withCredentials: true,
     };
 
@@ -139,6 +134,29 @@ const Form = () => {
                     {errors.price?.message}
                   </div>
                 </div>
+
+                <div className="mb-4">
+                  <input
+                    {...register('imgUrl', {
+                      required: 'Campo obrigatório',
+                      pattern: {
+                        value:/^(https?|chrome):\/\/[^\s$.?#].[^\s]*$/gm,
+                        message: 'Deve ser uma URL válida'
+                      }
+                    })}
+                    type="text"
+                    className={`form-control base-input ${
+                      errors.name ? 'is-invalid' : ''
+                    }`}
+                    placeholder="URL da imagem do produto"
+                    name="imgUrl"
+                  />
+                  <div className="invalid-feedback d-block">
+                    {errors.imgUrl?.message}
+                  </div>
+                </div>
+
+
               </div>
             </div>
             <div className="col-lg-6">
