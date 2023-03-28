0c18f36bc9664512bf9ef6d036a7ed407f56b7a8
Luiz Carlos Silvestrini <luizsilvestrini15@hotmail.com>
2023-03-27 22:23:04 -0300

    Refactor toke, Role

--- frontend/src/util/__tests__/formatters.spec.ts
+++ frontend/src/util/__tests__/formatters.spec.ts
@@ -1,10 +1,44 @@
 import { formatPrice } from "util/formatters";
 
-test('formatPrice should format number pt-BR when given 10.1', () =>{
 
-    const value = 10.10;
+describe('formatPrice for positive numbers',() => {
+    test('formatPrice should format number pt-BR when given 10.1', () =>{
 
-    const result = formatPrice(value);
+        const value = 10.10;
+    
+        const result = formatPrice(value);
+    
+        expect(result).toEqual("10,10");
+    });
 
-    expect(result).toEqual("10,10");
+        test('formatPrice should format number pt-BR when given 0.1', () =>{
+
+            const value = 0.1;
+        
+            const result = formatPrice(value);
+        
+            expect(result).toEqual("0,10");
+        });
+
+});
+
+describe('formatPrice for non-ositive numbers',() => {
+    test('formatPrice should format number pt-BR when given 0', () =>{
+
+        const value = 0;
+    
+        const result = formatPrice(value);
+    
+        expect(result).toEqual("0,00");
+    });
+
+        test('should format number pt-BR when given -5.01', () =>{
+
+            const value = -5.01;
+        
+            const result = formatPrice(value);
+        
+            expect(result).toEqual("-5,01");
+        });
+
 });
